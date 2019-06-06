e8d2ddd2c57ecb42f5aefd67df9b998fa72bdf33
phyzerbert <xian1017@outlook.com>
2019-06-06 22:45:01 +0800

    upload source

--- app/Exports/ProjectExport.php
+++ app/Exports/ProjectExport.php
@@ -8,29 +8,35 @@ use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use DB;
 
-class ProjectExport implements FromCollection, WithHeadings
+class ProjectExport implements FromArray, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function array(): array
     {
         $data = DB::table('projects')
                 ->join('companies', 'projects.company_id', '=', 'companies.id')
                 ->join('users', 'projects.user_id', '=', 'users.id')
-                ->select('projects.name', 'projects.description', 'users.name as username', 'companies.name as companyname', 'projects.due_to', 'projects.limit', 'projects.created_at')
-                ->get();
+                ->select('projects.id','projects.name', 'projects.description', 'users.name as username', 'companies.name as companyname', 'projects.due_to', 'projects.progress', 'projects.limit', 'projects.created_at')
+                ->get()->toArray();
+        foreach ($data as $key => $value) {
+            $project = Project::find($value->id);
+            $value->progress = $project->courses->avg('progress');
+        }
         return $data;
     }
 
     public function headings(): array
     {
         return [
+            'No',
             'Name',
             'Description',
             'User',
             'Company',
             'Due Date',
+            'Progress',
             'Money Limit',
             'Created Date',
         ];
