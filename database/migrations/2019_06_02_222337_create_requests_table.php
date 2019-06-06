22971560e7c43fdf75efa3084b85ed47e581ebd8
phyzerbert <xian1017@outlook.com>
2019-06-06 21:15:38 +0800

    upload source

--- database/migrations/2019_06_02_222337_create_requests_table.php
+++ database/migrations/2019_06_02_222337_create_requests_table.php
@@ -20,6 +20,7 @@ class CreateRequestsTable extends Migration
             $table->integer('user_id')->nullable();
             $table->integer('course_id')->nullable();
             $table->float('amount', 8, 2)->default(0);
+            $table->string('note')->nullable();
             $table->integer('exceed')->default(0);
             $table->string('attachment')->nullable();
             $table->date('request_date')->nullable();
